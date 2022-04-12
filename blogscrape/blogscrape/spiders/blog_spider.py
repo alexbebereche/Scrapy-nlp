1a6b92ce8d4b8505708cd314117d308bec4a605d
Alex Bebereche <alexbebereche99@yahoo.com>
2022-04-12 12:17:21 +0300

    adding spider

---
+++ blogscrape/blogscrape/spiders/blog_spider.py
@@ -0,0 +1,23 @@
+from gettext import textdomain
+from operator import ne
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = "blog"
+    
+    start_urls = [
+        "https://www.zyte.com/blog/",
+    ]
+    
+    def parse(self, response):
+        for item in response.css('a.oxy-post-title::text').getall():
+            yield{
+                "title":item
+            }
+            
+        next_page = response.css("a.next.page-numbers::attr(href)").get()
+        if next_page != None:
+            next_page = response.urljoin(next_page)
+            
+            yield scrapy.Request(next_page, callback=self.parse)
---
+++ blogscrape/main.py
@@ -0,0 +1,27 @@
+import nltk
+import json
+
+def extract_json(file_name: str) -> set:
+    # read file
+    with open(file_name, "r") as jf:
+        json_data = json.load(jf)
+
+    # create a set, because there is an article that appears on every page
+    title_set = set()
+
+    # traverse json file
+    for json_item in json_data:
+        title_set.add(json_item["title"])  
+    
+    return title_set
+
+if __name__ == "__main__":
+    title_set =  extract_json("blog.json")
+    title_str = str(title_set)   
+    
+    all_words = nltk.tokenize.word_tokenize(title_str)
+
+    stopwords = nltk.corpus.stopwords.words('english')
+    allWordExceptStopDist = nltk.FreqDist(w.lower() for w in all_words if (w not in stopwords) and (w.isalpha() == True)) # could add len condition to exclude words like 'a'
+
+    print(allWordExceptStopDist.most_common(10))
